9002975756027d670162721daa1825d5980905c6
Mykhaylo Komarichyn <mykhaylo.komarichyn@unicorn.com>
2021-12-13 23:27:59 +0100

    some configs

--- src/main/java/org/komarichyn/telegram/bot/SmartHouseBot.java
+++ src/main/java/org/komarichyn/telegram/bot/SmartHouseBot.java
@@ -1,23 +1,12 @@
 package org.komarichyn.telegram.bot;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
-import org.telegram.telegrambots.meta.TelegramBotsApi;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
-import org.telegram.telegrambots.meta.exceptions.TelegramApiRequestException;
 
-/**
- * Done! Congratulations on your new bot. You will find it at t.me/MK_SmartHouseBot. You can now add a description, about section and profile picture for your bot, see /help for a list of commands. By
- * the way, when you've finished creating your cool bot, ping our Bot Support if you want a better username for it. Just make sure the bot is fully operational before you do this.
- * <p>
- * Use this token to access the HTTP API: 5010264557:AAHN5GtTLQTZEha_co-8A0Ntb1NvmIc_25U Keep your token secure and store it safely, it can be used by anyone to control your bot.
- * <p>
- * For a description of the Bot API, see this page: https://core.telegram.org/bots/api
- */
+
 @Slf4j
 public class SmartHouseBot extends TelegramLongPollingBot {
 
---
+++ src/test/java/org/komarichyn/ss/SensorServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.komarichyn.ss;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SensorServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
